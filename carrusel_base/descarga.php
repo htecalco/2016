9792c76361618a5bcd7c66331980ff80b0cc4410
Hilda Tecalco Vinas <htecalco@yahoo.com.mx>
2016-06-01 14:21:20 -0700

    descarga de los 3 json y guardados en carpeta

--- carrusel_base/descarga.php
+++ carrusel_base/descarga.php
@@ -1,7 +1,7 @@
 <?php
-	$error=false;
+$error=false;
 $ch = curl_init();
-$source = "http://api.preppuebla.org/gobernador/carrusel";
+$source = "http://api.prepdurango.org/gobernador/carrusel";
 curl_setopt($ch, CURLOPT_URL, $source);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 10);//tiempo en segundos
@@ -15,11 +15,56 @@ if(curl_errno($ch))
 curl_close($ch);
 
 if (!$error){
-	$destination = "./gobernador.json";
+	$destination = "json/gobernador.json";
 	$file = fopen($destination, "w+");
 	fputs($file, $data);
 	fclose($file);
 }
+
+$error=false;
+$ch = curl_init();
+$source = "http://api.prepdurango.org/diputados/carrusel";
+curl_setopt($ch, CURLOPT_URL, $source);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);//tiempo en segundos
+$data = curl_exec($ch);
+if(curl_errno($ch))
+{
+    echo 'Curl error: ' . curl_error($ch);
+	$error=true;
+}
+//var_dump(curl_getinfo($ch));
+curl_close($ch);
+
+if (!$error){
+	$destination = "json/diputados.json";
+	$file = fopen($destination, "w+");
+	fputs($file, $data);
+	fclose($file);
+}
+
+$error=false;
+$ch = curl_init();
+$source = "http://api.prepdurango.org/ayuntamientos/carrusel";
+curl_setopt($ch, CURLOPT_URL, $source);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);//tiempo en segundos
+$data = curl_exec($ch);
+if(curl_errno($ch))
+{
+    echo 'Curl error: ' . curl_error($ch);
+	$error=true;
+}
+//var_dump(curl_getinfo($ch));
+curl_close($ch);
+
+if (!$error){
+	$destination = "json/ayuntamientos.json";
+	$file = fopen($destination, "w+");
+	fputs($file, $data);
+	fclose($file);
+}
+
 //$string = file_get_contents("elecciones.json");
 //$todo = json_decode($string, true);
 //$entidades=$todo['data'];
